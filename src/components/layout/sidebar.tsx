c4222c49edff95d1a46f6193d2b6f4a660e4a7eb
Kevin Custer <kevin@kevin-custer.com>
2026-04-26 21:20:44 -0400

    Final updates for rename

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -71,7 +71,7 @@ export function Sidebar({ user, counts, mobile, onNavigate }: SidebarProps) {
           onClick={onNavigate}
           className="text-xl font-bold text-white"
         >
-          Curiolu
+          curiolu
         </Link>
       </div>
 
